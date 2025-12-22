6ba96c4a1f132bdcc556d77c5799133b786efdc8
teeportal5 <teeportal5@gmail.com>
2025-12-22 16:17:51 +0300

    Create ui.js

---
+++ modules/ui.js
@@ -0,0 +1,20 @@
+// modules/ui.js
+class UIManager {
+    showToast(message, type = 'info') {
+        // Toast notification
+    }
+    
+    showLoading(show = true) {
+        // Loading indicator
+    }
+    
+    updateDashboardStats(stats) {
+        // Update dashboard
+    }
+    
+    populateDropdown(selectId, data) {
+        // Populate dropdowns
+    }
+    
+    // Add more UI helper methods
+}
